1b8fce4d1770ff060a4537d6a77a54e3dd020e9d
saccharios <saccharios@gmail.com>
2019-01-06 18:17:19 +0100

    Debug working in eclipse
    Create a new run config in eclipse. C/C++ Application. Point to the exe
    in devel/lib/<pckg-name>/<node-name>. Changed in the CMakeList to always
    use debug build. Debug must be start witht the debug running, but no
    other stuff is needed. Eclipse was started from terminal, so the
    enivoronment was properly setup.

--- src/main_window_test.cpp
+++ src/main_window_test.cpp
@@ -7,8 +7,9 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "main_window_test");
   ros::NodeHandle nh;
 
-  ROS_INFO("Hello world!");
-
+  ROS_INFO("Hello world aa!");
+  ROS_INFO("Hello world bb!");
+  ROS_INFO("Hello world cc!");
 
   QApplication a(argc, argv);
   MainWindow w;
